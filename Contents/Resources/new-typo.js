94da1825451ff3dc2b5787fb93c07bd5c8b7c54e
James Bradford <jabradf@c02xv0rujgh7.reicorpnet.com>
2019-07-18 15:04:23 -0700

    moving resources

---
+++ Contents/Resources/new-typo.js
@@ -0,0 +1,170 @@
+module.exports = [
+
+    // HEADINGS //
+    {
+        name: 'cdr-text-heading-800',
+        value: {
+            fontFamily: 'REI Stuart App',
+            fontWeight: 10,
+            fontSize: 48,
+            lineHeight: 60,
+            spacing: 0.01,
+        },
+    },
+    {
+        name: 'cdr-text-heading-700',
+        value: {
+            fontFamily: 'REI Stuart App',
+            fontWeight: 10,
+            fontSize: 38,
+            lineHeight: 48,
+            spacing: 0.0075,
+        },
+    },
+    {
+        name: 'cdr-text-heading-600',
+        value: {
+            fontFamily: 'REI Stuart App',
+            fontWeight: 10,
+            fontSize: 32,
+            lineHeight: 40,
+            spacing: 0.005,
+        },
+    },
+    {
+        name: 'cdr-text-heading-500',
+        value: {
+            fontFamily: 'REI Stuart App',
+            fontWeight: 10,
+            fontSize: 28,
+            lineHeight: 32,
+            spacing: -0.0025,
+        },
+    },
+    {
+        name: 'cdr-text-heading-400',
+        value: {
+            fontFamily: 'REI Stuart App',
+            fontWeight: 10,
+            fontSize: 24,
+            lineHeight: 32,
+            spacing: 0,
+        },
+    },
+    {
+        name: 'cdr-text-heading-300',
+        value: {
+            fontFamily: 'REI Stuart App',
+            fontWeight: 10,
+            fontSize: 20,
+            lineHeight: 26,
+            spacing: 0.005,
+        },
+    },
+    {
+        name: 'cdr-text-heading-200',
+        value: {
+            fontFamily: 'REI Stuart App',
+            fontWeight: 10,
+            fontSize: 18,
+            lineHeight: 24,
+            spacing: 0.01,
+        },
+    },
+    {
+        name: 'cdr-text-heading-100',
+        value: {
+            fontFamily: 'REI Stuart App',
+            fontWeight: 10,
+            fontSize: 14,
+            lineHeight: 20,
+            spacing: 0.015,
+        },
+    },
+
+    // DAT BOD //
+    {
+        name: 'cdr-text-body-400',
+        value: {
+            fontFamily: 'REI Stuart App',
+            fontWeight: 6,
+            fontSize: 20,
+            lineHeight: 32,
+            spacing: 0,
+        },
+    },
+    {
+        name: 'cdr-text-body-300',
+        value: {
+            fontFamily: 'REI Stuart App',
+            fontWeight: 6,
+            fontSize: 18,
+            lineHeight: 28,
+            spacing: 0,
+        },
+    },
+    {
+        name: 'cdr-text-body-200',
+        value: {
+            fontFamily: 'REI Stuart App',
+            fontWeight: 6,
+            fontSize: 16,
+            lineHeight: 24,
+            spacing: 0,
+        },
+    },
+    {
+        name: 'cdr-text-body-sans-400',
+        value: {
+            fontFamily: 'Graphik App',
+            fontWeight: 6,
+            fontSize: 20,
+            lineHeight: 32,
+            spacing: 0,
+        },
+    },
+    {
+        name: 'cdr-text-body-sans-300',
+        value: {
+            fontFamily: 'Graphik App',
+            fontWeight: 6,
+            fontSize: 18,
+            lineHeight: 28,
+            spacing: 0,
+        },
+    },
+    {
+        name: 'cdr-text-body-sans-200',
+        value: {
+            fontFamily: 'Graphik App',
+            fontWeight: 6,
+            fontSize: 16,
+            lineHeight: 24,
+            spacing: 0,
+        },
+    },
+    {
+        name: 'cdr-text-body-sans-100',
+        value: {
+            fontFamily: 'Graphik App',
+            fontWeight: 6,
+            fontSize: 14,
+            lineHeight: 20,
+            spacing: 0,
+        },
+    },
+
+    // UTIL //
+    {
+        name: 'cdr-text-heading-label-150',
+        value: {
+            fontFamily: 'Graphik App',
+            fontWeight: 8,
+            fontSize: 12,
+            lineHeight: 16,
+            spacing: 0,
+            textTransform: 'uppercase',
+        },
+    },
+
+]
