e261c628b7de0c44ec356634587d77d04a9efccc
James Bradford <jabradf@rei.com>
2019-08-07 11:39:49 -0700

    naming so ordering works well

--- src/Resources/sketch-paths-map.js
+++ src/Resources/sketch-paths-map.js
@@ -7,48 +7,49 @@ import { PATHS } from "../Partials/constants";
 export default {
 
     "cdr-text-heading-700": [
-        [PATHS.contextualUsages, "Page Title", PATHS.screenSmall],
-        [PATHS.contextualUsages, "Section Heading", PATHS.screenLarge]
+        [PATHS.contextualUsages, "609 Page Title", PATHS.screenSmall],
+        [PATHS.contextualUsages, "409 Section Heading", PATHS.screenLarge]
     ],
     "cdr-text-heading-800": [
-        [PATHS.contextualUsages, "Page Title", PATHS.screenLarge]
+        [PATHS.contextualUsages, "609 Page Title", PATHS.screenLarge]
     ],
 
     "cdr-text-subheading-300": [
-        [PATHS.contextualUsages, "Subtitle", PATHS.screenSmall]
+        [PATHS.contextualUsages, "608 Subtitle", PATHS.screenSmall]
     ],
     "cdr-text-subheading-400": [
-        [PATHS.contextualUsages, "Subtitle", PATHS.screenLarge],
-        [PATHS.contextualUsages, "Subheadline", PATHS.screenSmall]
+        [PATHS.contextualUsages, "608 Subtitle", PATHS.screenLarge],
+        [PATHS.contextualUsages, "808 Subheadline", PATHS.screenSmall]
     ],
 
     "cdr-text-heading-600": [
-        [PATHS.contextualUsages, "Section Heading", PATHS.screenSmall]
+        [PATHS.contextualUsages, "409 Section Heading", PATHS.screenSmall]
     ],
-    // "cdr-text-heading-700" : [PATHS.screenLarge]
+    // "cdr-text-heading-700" : []
 
     "cdr-text-heading-400": [
-        [PATHS.contextualUsages, "Subsection Heading", PATHS.screenSmall],
-        [PATHS.contextualUsages, "Sub-subsection Heading", PATHS.screenLarge]
+        [PATHS.contextualUsages, "408 Subsection Heading", PATHS.screenSmall],
+        [PATHS.contextualUsages, "407 Sub-subsection Heading", PATHS.screenLarge]
     ],
     "cdr-text-heading-500": [
-        [PATHS.contextualUsages, "Subsection Heading", PATHS.screenLarge]
+        [PATHS.contextualUsages, "408 Subsection Heading", PATHS.screenLarge]
     ],
 
     "cdr-text-heading-300": [
-        [PATHS.contextualUsages, "Sub-subsection Heading", PATHS.screenSmall]
+        [PATHS.contextualUsages, "407 Sub-subsection Heading", PATHS.screenSmall]
     ],
+    // "cdr-text-heading-400": []
 
     "cdr-text-display-800": [
-        [PATHS.contextualUsages, "Headline", PATHS.screenSmall],
+        [PATHS.contextualUsages, "809 Headline", PATHS.screenSmall],
     ],
     "cdr-text-display-900": [
-        [PATHS.contextualUsages, "Headline", PATHS.screenLarge],
+        [PATHS.contextualUsages, "809 Headline", PATHS.screenLarge],
     ],
 
-    // "cdr-text-subheading-400" : [PATHS.screenSmall]
+    // "cdr-text-subheading-400" : []
     "cdr-text-subheading-500": [
-        [PATHS.contextualUsages, "Subheadline", PATHS.screenLarge],
+        [PATHS.contextualUsages, "808 Subheadline", PATHS.screenLarge],
     ],
 
     // "cdr-text-label-100": [ // doesn't exist yet
