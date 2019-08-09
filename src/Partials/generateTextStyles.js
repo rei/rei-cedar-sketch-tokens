0d265a2ce066e4748b91654959221752f36d7cfd
James Bradford <jabradf@rei.com>
2019-08-09 15:18:55 -0700

    doin it

--- src/Partials/generateTextStyles.js
+++ src/Partials/generateTextStyles.js
@@ -9,54 +9,55 @@ export default function generateTextStyles(textTokens, colorTokens) {
     const textStyles = []
     const textColorTokens = colorTokens.filter(color => color.type === 'text')
     textTokens.forEach(textToken => {
-        const isDefaultText = textToken.name.includes('default')
+        // const isDefaultText = textToken.name.includes('default')
 
-        textColorTokens.forEach(textColorToken => {
-            // textAlignment.forEach(textAlign => {
-            const textColorPath = tokenToArray(textColorToken.name, 3)
-            const textTokenPath = tokenToArray(textToken.name, 2)
-            const textStylePath = [textTokenPath, textColorPath].flat() // [textAlign.name, PATHS.gridOptions, textTokenPath, textColorPath].flat()
-            const tokenNames = [textColorToken.name, textToken.name]
-            // if (textAlign.css != '') tokenNames.push(textAlign.css)
+        // textColorTokens.forEach(textColorToken => {
+        // textAlignment.forEach(textAlign => {
+        // const textColorPath = tokenToArray(textColorToken.name, 3)
+        const textTokenPath = tokenToArray(textToken.name, 2)
+        // const textStylePath = [textTokenPath, textColorPath].flat() // [textAlign.name, PATHS.gridOptions, textTokenPath, textColorPath].flat()
+        // const tokenNames = [textColorToken.name, textToken.name]
+        // if (textAlign.css != '') tokenNames.push(textAlign.css)
 
-            const style = {
-                lineHeight: textToken.value.lineHeight,
-                fontSize: textToken.value.fontSize,
-                fontFamily: textToken.value.fontFamily,
-                fontWeight: fontWeightTableLookup(textToken.value.fontFamily, textToken.value.fontWeightOriginal),
-                textTransform: textToken.value.textTransform,
-                textColor: textColorToken.value,
-                alignment: Text.Alignment.left, // textAlign.value
-                borders: []
-            }
+        const style = {
+            lineHeight: textToken.value.lineHeight,
+            fontSize: textToken.value.fontSize,
+            fontFamily: textToken.value.fontFamily,
+            fontWeight: fontWeightTableLookup(textToken.value.fontFamily, textToken.value.fontWeightOriginal),
+            // textTransform: textToken.value.textTransform,
+            textColor: '#AAAAAA', // textColorToken.value,
+            alignment: Text.Alignment.left, // textAlign.value,
+            borders: []
+        }
 
-            textStyles.push({
-                name: createSketchPathTwo(textTokenPath, textColorPath, tokenNames),
-                style: style
-            })
-
-            if (sketchPathMap[textToken.name] != null) {
-                sketchPathMap[textToken.name].forEach((sketchPath, i) => {
-                    textStyles.push({
-                        name: createSketchPathTwo(
-                            sketchPath,
-                            textColorPath,
-                            tokenNames.concat([`ex${i}`])),
-                        style: style
-                    })
-                })
-            }
-
-            if (isDefaultText && textColorToken.name.includes('link')) {
-                textStyles.push(cdrTextLinkStyle({
-                    name: createSketchPathTwo(textTokenPath, textColorPath, tokenNames),
-                    style: style
-                }))
-            }
-
-            // })
+        textStyles.push({
+            name: createSketchPathTwo(textTokenPath, [], textToken.name),
+            style: style
         })
+
+        // if (sketchPathMap[textToken.name] != null) {
+        //     sketchPathMap[textToken.name].forEach((sketchPath, i) => {
+        //         textStyles.push({
+        //             name: createSketchPathTwo(
+        //                 sketchPath,
+        //                 [],
+        //                 [textTokenPath.name, `ex${i}`]
+        //             ),
+        //             style: style
+        //         })
+        //     })
+        // }
+
+        // if (isDefaultText && textColorToken.name.includes('link')) {
+        //     textStyles.push(cdrTextLinkStyle({
+        //         name: createSketchPathTwo(textTokenPath, textColorPath, tokenNames),
+        //         style: style
+        //     }))
+        // }
+
     })
+    // })
+    // })
     return textStyles
 }
 
@@ -66,14 +67,14 @@ const textAlignment = [
         value: Text.Alignment.left,
         css: '' // 'cdr-text-align-left' is default css
     },
-    {
-        name: 'Center',
-        value: Text.Alignment.center,
-        css: 'cdr-text-align-center'
-    },
-    {
-        name: 'Right',
-        value: Text.Alignment.right,
-        css: 'cdr-text-align-right'
-    },
+    // {
+    //     name: 'Center',
+    //     value: Text.Alignment.center,
+    //     css: 'cdr-text-align-center'
+    // },
+    // {
+    //     name: 'Right',
+    //     value: Text.Alignment.right,
+    //     css: 'cdr-text-align-right'
+    // },
 ]
